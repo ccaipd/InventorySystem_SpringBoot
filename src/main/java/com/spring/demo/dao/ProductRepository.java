a70fe526f22af6aa5a6cdf6a0e7582c6792c4d78
Liming Ding <liming_ding@apple.com>
2022-08-09 15:24:42 -0700

    spring boot practice

--- src/main/java/com/spring/demo/dao/ProductRepository.java
+++ src/main/java/com/spring/demo/dao/ProductRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.spring.demo.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(value = "SELECT * FROM product_inventory WHERE product_name = ?1", nativeQuery = true)
